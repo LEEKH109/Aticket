566c2323c6f5800fd4923705a3037e38d12ef9ae
박이언 <0onionion0@naver.com>
2024-01-19 13:56:28 +0900

    feat(be): BaseEntity 추가

---
+++ server/src/main/java/me/articket/server/common/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package me.articket.server.common.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+}
