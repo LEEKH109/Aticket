6c7ddac29ecb176aa308e4ad5744b9ff3ab604e3
leekunhee <dlrjsgml109@gmail.com>
2024-01-22 13:11:28 +0900

    chore(fe): 푸터 컴포넌트 추가 및 기타세팅 수정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import Button from '@mui/material/Button';
 import './App.css'
-import theme from './util/theme';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <Button variant="contained">Hello World</Button>
+      <div className="relative mx-auto w-[412px] bg-slate-900 h-[100vh]">
+        <div className="px-[5%]">
+          <h1 className="text-white text-2xl">test</h1>
+        </div>
+        <Footer/>
+      </div>
     </>
   )
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return(
+        <>
+            <div className="fixed w-full h-16 bottom-0">
+            <h1 className="text-white text-2xl">footer</h1>
+            </div>
+        </>
+    );
+}
+
+export default Footer;
