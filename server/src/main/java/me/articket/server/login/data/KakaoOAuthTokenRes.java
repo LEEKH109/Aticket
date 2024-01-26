fbfeb582b70091f8d364bf6799485d018b703796
강지헌 <wlgjs0458@naver.com>
2024-01-26 15:32:04 +0900

    refactor(be): 목적에 맞게 이름 변경

--- server/src/main/java/me/articket/server/login/data/KakaoOAuthTokenRes.java
+++ server/src/main/java/me/articket/server/login/data/KakaoOAuthTokenRes.java
@@ -3,11 +3,11 @@ package me.articket.server.login.data;
 import lombok.Data;
 
 @Data
-public class OAuthToken { //OAuth 데이터를 담을 Object
+public class KakaoOAuthTokenRes { //OAuth 데이터를 담을 Object
     private String access_token;
     private String token_type;
     private String refresh_token;
     private int expires_in;
-    private String scope;
     private int refresh_token_expires_in;
+    private String scope;
 }
