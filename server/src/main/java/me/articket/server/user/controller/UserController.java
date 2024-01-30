da191651b5a6600dea7ae99dcb3e5c9f58355fbe
강지헌 <wlgjs0458@naver.com>
2024-01-30 15:18:40 +0900

    test(be): jwt, OAuth2 로그인 테스트 구현

--- server/src/main/java/me/articket/server/user/controller/UserController.java
+++ server/src/main/java/me/articket/server/user/controller/UserController.java
@@ -2,8 +2,10 @@ package me.articket.server.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import me.articket.server.common.response.SuccessResponse;
+import me.articket.server.login.data.UserDetail;
 import me.articket.server.user.data.UserRes;
 import me.articket.server.user.service.UserService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +20,13 @@ public class UserController {
 
     private final UserService userService;
 
-
-    @GetMapping
-    @RequestMapping("/{id}")
+    @GetMapping("/{id}")
     public SuccessResponse<UserRes> getUser(@PathVariable Long id) {
         return new SuccessResponse<>(userService.getUser(id));
     }
 
-
+    @GetMapping("/test")
+    public String test(@AuthenticationPrincipal UserDetail details) {
+        return "니 닉네임은 '" + details.getNickname() + "' 입니다.";
+    }
 }
