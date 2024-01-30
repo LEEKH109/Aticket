9d269338f9f71d133381a2d9836a046615abde9c
박이언 <0onionion0@naver.com>
2024-01-30 15:31:19 +0900

    feat(be): Shorts 생성, 수정 API 추가

--- server/src/main/java/me/articket/server/shorts/controller/ShortsController.java
+++ server/src/main/java/me/articket/server/shorts/controller/ShortsController.java
@@ -2,12 +2,11 @@ package me.articket.server.shorts.controller;
 
 import lombok.RequiredArgsConstructor;
 import me.articket.server.common.response.SuccessResponse;
+import me.articket.server.shorts.data.AddShortsReq;
+import me.articket.server.shorts.data.ModifyShortsReq;
 import me.articket.server.shorts.data.ShortsInfoRes;
 import me.articket.server.shorts.service.ShortsService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/shorts")
@@ -19,5 +18,15 @@ public class ShortsController {
     @GetMapping("/{id}")
     public SuccessResponse<ShortsInfoRes> getShortsInfo(@PathVariable Long id) {
         return new SuccessResponse<>(shortsService.getShortsInfo(id));
+    }
+
+    @PostMapping("/add")
+    public SuccessResponse<ShortsInfoRes> addShorts(@RequestBody AddShortsReq req) {
+        return new SuccessResponse<>(shortsService.addShorts(req));
+    }
+
+    @PutMapping("/{id}")
+    public SuccessResponse<ShortsInfoRes> modifyShortsInfo(@PathVariable Long id, @RequestBody ModifyShortsReq req) {
+        return new SuccessResponse<>(shortsService.modifyShortsInfo(id, req));
     }
 }
