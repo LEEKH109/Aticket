63a77dcf186e4a9f60ee0c3bd5298e4c84492cd7
강지헌 <wlgjs0458@naver.com>
2024-01-31 17:33:44 +0900

    refactor(be): response 변경에 따른 userId 추가

--- server/src/main/java/me/articket/server/login/data/OauthTokenRes.java
+++ server/src/main/java/me/articket/server/login/data/OauthTokenRes.java
@@ -7,6 +7,7 @@ import lombok.Data;
 @Data
 @Builder
 public class OauthTokenRes {
+    private Long userId;
     private String tokenType;
     private String accessToken;
     private Integer expiresIn;
