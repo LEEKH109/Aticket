7c71a0d8e7e3091895e1fd52c126134a0314308c
강지헌 <wlgjs0458@naver.com>
2024-01-31 17:32:11 +0900

    refactor(be): 로그인 검증 예외 url 추가

--- server/src/main/java/me/articket/server/common/config/SecurityConfig.java
+++ server/src/main/java/me/articket/server/common/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -25,15 +26,22 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(AbstractHttpConfigurer::disable);
         http.exceptionHandling(authEntryPoint -> authEntryPoint
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .accessDeniedHandler(jwtAccessDeniedHandler));
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.authorizeHttpRequests(request -> request
                 .requestMatchers(
+
                         "/login/oauth2/code/kakao",
-                        "/shorts/*"
+                        "/login/oauth/token",
+                        "/shorts/*",
+                        "/chat/*/paging",
+                        "/chat/*/preview",
+                        "/chat/*/latest",
+                        "/chat/list",
+                        "/art/*"
 
                 ).permitAll()
                 .anyRequest().authenticated());
