5fae351f7ad626ccd21a873e5cce1e2ae172690c
강지헌 <wlgjs0458@naver.com>
2024-01-31 17:34:52 +0900

    feat(be): custom error code 추가

--- server/src/main/java/me/articket/server/common/exception/ErrorCode.java
+++ server/src/main/java/me/articket/server/common/exception/ErrorCode.java
@@ -9,6 +9,12 @@ public enum ErrorCode {
     KAKAO_AUTHORIZATION_CODE_ERROR(403, "DDE001", "잘못된 code입니다."),
 
     USER_NOT_FOUND_ERROR(404, "USR001", "존재하지 않는 사용자입니다."),
+
+    ACCESS_TOKEN_EXPIRE_ERROR(401, "REF001", "Refresh Token의 기간이 만료되었습니다."),
+
+    ACCESS_TOKEN_ERROR(401, "REF002", "Refresh Token이 잘못되었습니다."),
+
+    REFRESH_TOKEN_VALIDATION_ERROR(401, "REF001", "Refresh Token이 잘못되었습니다."),
     ;
 
     private final int status;
