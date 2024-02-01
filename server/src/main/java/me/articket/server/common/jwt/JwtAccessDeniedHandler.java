098f158f2a2db8c690d0b8cba85cc1329b9f8853
박이언 <0onionion0@naver.com>
2024-02-01 13:35:28 +0900

    fix(be): 인증/인가 오류 응답 형식 JSend에 맞게 수정

--- server/src/main/java/me/articket/server/common/jwt/JwtAccessDeniedHandler.java
+++ server/src/main/java/me/articket/server/common/jwt/JwtAccessDeniedHandler.java
@@ -1,7 +1,10 @@
 package me.articket.server.common.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import me.articket.server.common.response.FailResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -11,9 +14,14 @@ import java.io.IOException;
 @Component
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
         // 필요한 권한이 존재하지 않은 경우 403 FORBIDDEN 에러를 리턴
-        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        objectMapper.writeValue(response.getOutputStream(), FailResponse.of("403", "접근 권한이 없습니다."));
     }
 }
