19ff194f3a3cc1005c0c806044e9fa91a4c81a4a
이건희 <dlrjsgml109@gmail.com>
2024-02-03 17:52:40 +0900

    feat(be): 결제 사전 정보 조회를 위한 seat,timetable repositoy 메소드 추가 및 seat service 메소드 추가

--- vendor/src/main/java/me/articket/vendor/seat/repository/SeatRepository.java
+++ vendor/src/main/java/me/articket/vendor/seat/repository/SeatRepository.java
@@ -1,8 +1,10 @@
 package me.articket.vendor.seat.repository;
 
 import java.util.List;
+import me.articket.vendor.seat.data.SeatReservationInfoDto;
 import me.articket.vendor.seat.data.SeatTotalDtoRes;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
@@ -10,8 +12,26 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SeatRepository {
 
+  // 시간표 기준으로 예약가능한 좌석 조회 메서드
   @Select("SELECT timetable_id as timetableId, seat_number as seatNumber, status, type, price " +
       "FROM seat " +
       "WHERE timetable_id = #{timetableId}")
   List<SeatTotalDtoRes.SeatDtoRes> findSeatByTimetableId(int timetableId);
+
+  // 예약 요청 기준으로 DB에서 일괄 조회 메서드
+  @Select("<script>" +
+      "SELECT a.art_id, a.title AS artTitle, t.timetable_id, t.start_time, t.end_time, s.seat_number, s.status, s.price " +
+      "FROM art a " +
+      "JOIN timetable t ON a.art_id = t.art_id " +
+      "JOIN seat s ON t.timetable_id = s.timetable_id " +
+      "WHERE a.art_id = #{artId} AND t.timetable_id = #{timetableId} " +
+      "AND s.seat_number IN " +
+      "<foreach item='item' collection='seatNumbers' open='(' separator=',' close=')'>" +
+      "#{item}" +
+      "</foreach>" +
+      "</script>")
+  List<SeatReservationInfoDto.SeatInfo> findSeatReservationInfo(
+      @Param("artId") int artId,
+      @Param("timetableId") int timetableId,
+      @Param("seatNumbers") List<String> seatNumbers);
 }
