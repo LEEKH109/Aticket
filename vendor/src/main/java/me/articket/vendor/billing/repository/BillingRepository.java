78ce29dd135aa94348eb41167317e6503937c6ca
이건희 <dlrjsgml109@gmail.com>
2024-02-04 16:44:41 +0900

    feat(be): Billing, BillinDetail 생성 수정 repository 작성

--- vendor/src/main/java/me/articket/vendor/billing/repository/BillingRepository.java
+++ vendor/src/main/java/me/articket/vendor/billing/repository/BillingRepository.java
@@ -1,41 +1,39 @@
 package me.articket.vendor.billing.repository;
 
+import me.articket.vendor.billing.domain.Billing;
+import me.articket.vendor.billing.domain.BillingDetail;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
 @Mapper
 @Repository
 public interface BillingRepository {
 
-  @Select("SELECT COUNT(*) " +
-      "FROM art " +
-      "WHERE art_id = #{artId})")
-  int countValidArtId(@Param("artId") int artId);
+  // 결제 객체 생성, tid와 pg_token은 결제 진행 중 생성되는 값이라 null로 둡니다.
+  @Insert("INSERT INTO billing (art_id, reservation_id, booker_name, status, category) " +
+      "VALUES (#{artId}, #{reservationId}, #{bookerName}, #{status}, #{category})")
+  @Options(useGeneratedKeys = true, keyProperty = "billingId")
+  int insertBilling(Billing billing);
 
-  @Select("SELECT COUNT(*) " +
-      "FROM timetable " +
-      "WHERE timetable_id = #{timetableId}")
-  int countValidTimetableId(@Param("timetableId") int timetableId);
+  // 전시를 위한 BillingDetail 생성(삽입)
+  @Insert({
+      "INSERT INTO billing_detail (billing_id, timetable_id, ticket_type_id, count) " +
+      "VALUES (#{billingId}, #{timetableId}, #{ticketTypeId}, #{count})"
+  })
+  int insertBillingDetailForExhibition(BillingDetail billingDetail);
 
-  @Select("SELECT COUNT(*) " +
-      "FROM ticket_type " +
-      "WHERE ticket_type_id = #{ticketTypeId}")
-  int countValidTicketTypeId(@Param("ticketTypeId") int ticketTypeId);
+  // 공연을 위한 BillingDetail 생성(삽입)
+  @Insert({
+      "INSERT INTO billing_detail (billing_id, timetable_id, seat_id, count) " +
+      "VALUES (#{billingId}, #{timetableId}, #{seatId}, #{count})"
+  })
+  int insertBillingDetailForPerformance(BillingDetail billingDetail);
 
-  //해당 좌석이 존재하고 예약이 가능한지 반환
-  @Select("SELECT " +
-      "CASE " +
-      "WHEN COUNT(*) > 0 THEN 'EXISTS_RESERVED' " +
-      "ELSE 'NOT_EXISTS' " +
-      "END AS seat_status " +
-      "FROM seat " +
-      "WHERE timetable_id = #{timetableId} " +
-      "AND seat_number = #{seatNumber}")
-  String checkSeatStatus(@Param("timetableId") int timetableId, @Param("seatNumber") String seatNumber);
-
-  @Insert()
+  // 결제 준비 완료시 tid 업데이트
+  @Update("UPDATE billing SET tid = #{tid} WHERE billing_id = #{billingId}")
+  int updateBillingWithTid(@Param("billingId") int billingId, @Param("tid") String tid);
 }
