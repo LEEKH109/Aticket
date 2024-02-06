8d67323fb9a38db3faf5c771f4dbf126500c91d4
강지헌 <wlgjs0458@naver.com>
2024-02-06 16:56:51 +0900

    feat(be): default profile url 추가

--- server/src/main/java/me/articket/server/user/controller/UserController.java
+++ server/src/main/java/me/articket/server/user/controller/UserController.java
@@ -3,9 +3,11 @@ package me.articket.server.user.controller;
 import lombok.RequiredArgsConstructor;
 import me.articket.server.common.response.SuccessResponse;
 import me.articket.server.user.data.NicknameReq;
+import me.articket.server.user.data.NicknameRes;
 import me.articket.server.user.data.ProfileUrlRes;
 import me.articket.server.user.data.UserRes;
 import me.articket.server.user.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,6 +18,9 @@ import org.springframework.web.multipart.MultipartFile;
 public class UserController {
 
     private final UserService userService;
+
+    @Value("${default-profile-url}")
+    private String defaultUrl;
 
     @GetMapping("/{id}")
     public SuccessResponse<UserRes> getUser(@PathVariable Long id) {
@@ -29,11 +34,11 @@ public class UserController {
 
     @DeleteMapping("/{id}/profile")
     public SuccessResponse<ProfileUrlRes> defaultProfileImage(@PathVariable Long id) {
-        return new SuccessResponse<>(userService.setProfileUrl(id, "https://articketbuket.s3.ap-northeast-2.amazonaws.com/defaultProfile.jpg"));
+        return new SuccessResponse<>(userService.setProfileUrl(id, defaultUrl));
     }
 
     @PutMapping("/{id}/nickname")
-    public SuccessResponse<UserRes> updateNickname(@PathVariable Long id, @RequestBody NicknameReq nickname) {
+    public SuccessResponse<NicknameRes> updateNickname(@PathVariable Long id, @RequestBody NicknameReq nickname) {
         return new SuccessResponse<>(userService.setNickname(id, nickname.getNickname()));
     }
 
