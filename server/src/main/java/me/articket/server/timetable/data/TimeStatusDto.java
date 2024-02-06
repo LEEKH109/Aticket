9d632478974207cccebf2537d50c6e891c3e03bb
이건희 <dlrjsgml109@gmail.com>
2024-02-07 07:37:21 +0900

    feat(be): 시간조회 기능 응답 객체dto 작성

--- server/src/main/java/me/articket/server/timetable/data/TimeStatusDto.java
+++ server/src/main/java/me/articket/server/timetable/data/TimeStatusDto.java
@@ -1,2 +1,10 @@
-package me.articket.server.timetable.data;public class TimeStatusDto {
+package me.articket.server.timetable.data;
+
+import lombok.Data;
+
+@Data
+public class TimeStatusDto {
+
+  private String date;
+  private boolean status;
 }
