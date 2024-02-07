c5faa3a1741055cf9e9059a0abd03fa129041444
이건희 <dlrjsgml109@gmail.com>
2024-02-07 23:33:56 +0900

    feat(be): 서버시간 기준 날짜 필터링 기능, 해당서버 시간 기준 시간 필터링 기능

--- vendor/src/main/java/me/articket/vendor/timetable/service/TimetableService.java
+++ vendor/src/main/java/me/articket/vendor/timetable/service/TimetableService.java
@@ -1,5 +1,8 @@
 package me.articket.vendor.timetable.service;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import me.articket.vendor.timetable.data.TimetableDtoRes;
@@ -14,11 +17,29 @@ public class TimetableService {
   private final TimetableRepository timetableRepository;
 
   public List<TimetableDtoRes> getTimetableByArtId(int artId) {
-    return timetableRepository.findTimetableByArtId(artId);
+    List<TimetableDtoRes> timetable = timetableRepository.findTimetableByArtId(artId);
+
+    List<TimetableDtoRes> filteredTimetable = timetable.stream()
+        .filter(t -> LocalDate.parse(t.getDate()).isAfter(LocalDate.now()) || LocalDate.parse(t.getDate()).isEqual(LocalDate.now()))
+        .toList();
+
+    return filteredTimetable;
   }
 
   public List<Timetable> getTimetableByDate(int artId, String date) {
-    return timetableRepository.findTimetableByDate(artId, date);
+    List<Timetable> timetables = timetableRepository.findTimetableByDate(artId, date);
+
+    LocalDate inputDate = LocalDate.parse(date, DateTimeFormatter.ISO_DATE);
+    LocalDate today = LocalDate.now();
+    LocalTime now = LocalTime.now();
+
+    List<Timetable> filteredTimetables = timetables.stream()
+        .filter(t -> {
+          LocalTime startTime = t.getStartTime().toLocalTime();//타입 변환
+          return !inputDate.equals(today) || (startTime.isAfter(now));
+        }).toList();
+
+    return filteredTimetables;
   }
 
 }
