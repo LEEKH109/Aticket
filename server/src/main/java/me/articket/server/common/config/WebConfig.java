2bdcf093af3d018b855bb64c2db71bd83fb745aa
강지헌 <wlgjs0458@naver.com>
2024-02-07 14:54:40 +0900

    feat(be): CORS 설정 추가

--- server/src/main/java/me/articket/server/common/config/WebConfig.java
+++ server/src/main/java/me/articket/server/common/config/WebConfig.java
@@ -23,6 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
                         HttpMethod.HEAD.name(),
                         HttpMethod.POST.name(),
                         HttpMethod.PUT.name(),
-                        HttpMethod.DELETE.name());
+                        HttpMethod.DELETE.name())
+                .allowCredentials(true);
     }
 }
