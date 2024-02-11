326f5e9ff6189ea5d5c0221d5be03acf6feead65
최지원 <jiwonq98@naver.com>
2024-02-12 04:30:49 +0900

    fix(be): websocket token 설정 수정

--- server/src/main/java/me/articket/server/common/config/WebSocketConfig.java
+++ server/src/main/java/me/articket/server/common/config/WebSocketConfig.java
@@ -1,7 +1,9 @@
 package me.articket.server.common.config;
 
 import lombok.RequiredArgsConstructor;
+import me.articket.server.common.jwt.StompHandler;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
@@ -9,6 +11,8 @@ import org.springframework.web.socket.config.annotation.*;
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer  {
+
+    private final StompHandler stompHandler;
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOrigins("http://i10a704.p.ssafy.io","http://i10a704.p.ssafy.io:8081","http://i10a704.p.ssafy.io:8082","http://i10a704.p.ssafy.io:80","http://localhost:5173","http://localhost:4173","https://articket.me").setAllowedOriginPatterns("*").withSockJS();
@@ -18,5 +22,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer  {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/chat");//채팅 보낼 때 앞에 chat을 붙이면 메시지브로커에게 도착
         registry.enableSimpleBroker("/room");//room이 붙은 경로에 대해 브로커 발동, sendTo에서 /room/어쩌구로 활용
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
     }
 }
