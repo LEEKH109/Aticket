b9de5b4632feb614a95a1474ff7651f59354b6a7
최지원 <jiwonq98@naver.com>
2024-02-13 02:52:26 +0900

    fix(be): websocket 경로 수정

--- server/src/main/java/me/articket/server/common/config/WebSocketConfig.java
+++ server/src/main/java/me/articket/server/common/config/WebSocketConfig.java
@@ -1,7 +1,7 @@
 package me.articket.server.common.config;
 
 import lombok.RequiredArgsConstructor;
-//import me.articket.server.common.jwt.StompHandler;
+import me.articket.server.common.jwt.StompHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -12,20 +12,23 @@ import org.springframework.web.socket.config.annotation.*;
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer  {
 
-//    private final StompHandler stompHandler;
+    private final StompHandler stompHandler;
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        System.out.println("registerStompEndpoints입니다");
         registry.addEndpoint("/ws").setAllowedOrigins("http://i10a704.p.ssafy.io","http://i10a704.p.ssafy.io:8081","http://i10a704.p.ssafy.io:8082","http://i10a704.p.ssafy.io:80","http://localhost:5173","http://localhost:4173","https://articket.me").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/chat");//채팅 보낼 때 앞에 chat을 붙이면 메시지브로커에게 도착
+        registry.setApplicationDestinationPrefixes("/send");//채팅 보낼 때 앞에 chat을 붙이면 메시지브로커에게 도착
         registry.enableSimpleBroker("/room");//room이 붙은 경로에 대해 브로커 발동, sendTo에서 /room/어쩌구로 활용
+        System.out.println("configureMessageBroker입니다");
     }
 
 //    @Override
 //    public void configureClientInboundChannel(ChannelRegistration registration) {
+//        System.out.println("configureClientInboundChannel입니다");
 //        registration.interceptors(stompHandler);
 //    }
 }
