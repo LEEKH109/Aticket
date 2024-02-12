e42a754810b2f813ea3425a5215d6c217428ae40
이건희 <dlrjsgml109@gmail.com>
2024-02-13 04:41:12 +0900

    fix(fe): api 변수 누락 수정

--- client/src/pages/book/BillingApprovePage.jsx
+++ client/src/pages/book/BillingApprovePage.jsx
@@ -1,17 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import { billingApi } from "../../util/billing-axios";
 import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import { LoginContext } from "../../components/LoginContext";
 
 const BillingApprovePage = () => {
   const { reservationId } = useParams();
   const [searchParams] = useSearchParams();
   const pgToken = searchParams.get("pg_token");
+  const { userId } = useContext(LoginContext);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (reservationId && pgToken) {
       billingApi
-        .approvePayment(reservationId, pgToken)
+        .approvePayment(reservationId, pgToken, userId)
         .then((response) => {
           navigate("/billing/result", {
             state: { paymentInfo: response.data },
