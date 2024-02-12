07eec4a2f3319e03d31f14b1f978d6b8338de2b1
최지원 <jiwonq98@naver.com>
2024-02-12 23:20:08 +0900

    fix(fe): onChatlogReceived 수정

--- client/src/pages/ChatRoomPage.jsx
+++ client/src/pages/ChatRoomPage.jsx
@@ -46,7 +46,11 @@ const ChatRoom = () => {
   const client = useRef(null);
 
   const onChatlogReceived = (message) => {
+    console.log("새 채팅이 들어왔다");
+    console.log("message: ");
+    console.log(message);
     const newChatlog = JSON.parse(message.body);
+    console.log(newChatlog);
     setPins((prevPins) => [...prevPins, newChatlog]);
     if (chatAreaRef.current) {
         const chatArea = chatAreaRef.current;
