1617bf1993daec4a6063d1990b62185888556941
이건희 <dlrjsgml109@gmail.com>
2024-02-13 09:21:42 +0900

    feat(fe): 배포를 위한 URL, props 수정

--- client/src/util/timetable-axios.js
+++ client/src/util/timetable-axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = `http://localhost:8081`;
+const BASE_URL = `http://i10a704.p.ssafy.io:8081`;
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
