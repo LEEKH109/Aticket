9a4d9d7cc6bc9d26fcb4118dab36b2427fb5460e
이건희 <dlrjsgml109@gmail.com>
2024-02-14 07:51:52 +0900

    feat(fe): 결제완료 페이지 데모

--- client/src/pages/book/BillingResultPage.jsx
+++ client/src/pages/book/BillingResultPage.jsx
@@ -1,12 +1,43 @@
 import { useLocation } from "react-router-dom";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
 const BillingResultPage = () => {
   const location = useLocation();
   const { paymentInfo } = location.state || {}; // state가 없는 경우를 대비한 기본값 설정
   return (
-    <div>
-      <h2>결제 정보</h2>
-      <pre>{JSON.stringify(paymentInfo, null, 2)}</pre>
+    <div className="flex flex-col h-[calc(100vh-64px)] overflow-auto">
+      <div className="w-full">
+        <div className="flex gap-0 px-5">
+          <ArrowBackIosIcon />
+          <div>Aːticket</div>
+        </div>
+        <div className="flex gap-0 px-5 text-xs font-bold text-center text-white mb-4">
+          <div className="justify-center px-6 py-2 whitespace-nowrap bg-black rounded-md border border-white border-solid ">
+            1.
+          </div>
+          <div className="justify-center px-6 py-2 whitespace-nowrap bg-black rounded-md border border-white border-solid ">
+            2.
+          </div>
+          <div className="grow justify-center px-12 py-2 bg-black rounded-md border border-white border-solid">
+            3. 결제 완료
+          </div>
+        </div>
+        <div className="mt-4 text-center">
+          <div className="text-lg font-semibold">{paymentInfo?.message}</div>
+          <div className="mt-4">
+            <div className="text-xs font-bold">주문 번호:</div>
+            <div className="text-sm">{paymentInfo?.partnerOrderId}</div>
+          </div>
+          <div className="mt-2">
+            <div className="text-xs font-bold">상품명:</div>
+            <div className="text-sm">{paymentInfo?.itemName}</div>
+          </div>
+          <div className="mt-2">
+            <div className="text-xs font-bold">승인 시각:</div>
+            <div className="text-sm">{formatDate(paymentInfo?.approvedAt)}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
