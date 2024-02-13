d988101c0d5a5ed13da23213cadac02982093001
YooJiwoong <yjw0835@naver.com>
2024-02-13 16:44:56 +0900

    fix(fe) : URL 수정

--- client/src/pages/ShortsPage.jsx
+++ client/src/pages/ShortsPage.jsx
@@ -20,7 +20,6 @@ const Shorts = () => {
   };
 
   useEffect(() => {
-    console.log("리스트를 ㅂ다앙ㅇ노머ㅏ용", shortList)
     if (userId) {
       ShortsAPI.getRecommendShortsList(category)
         .then(({ data }) => {
