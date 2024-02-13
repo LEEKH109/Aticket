d0f742838049b97aa3a73f30385d7faacaf3e989
이건희 <dlrjsgml109@gmail.com>
2024-02-14 07:28:35 +0900

    style(fe): 디자인 레이아웃 수정

--- client/src/pages/book/BillingPreviewPage.jsx
+++ client/src/pages/book/BillingPreviewPage.jsx
@@ -30,7 +30,7 @@ const BillingPreviewPage = () => {
     selectedSeats?.reduce((acc, seat) => acc + seat.price, 0) || 0;
 
   return (
-    <div className="flex flex-col items-center px-5 text-black max-w-[310px] mt-5">
+    <div className="flex flex-col h-[calc(100vh-64px)] overflow-auto">
       {showInfo && (
         <>
           <img
@@ -38,26 +38,24 @@ const BillingPreviewPage = () => {
             alt="Event Poster"
             className="w-32 h-auto mx-auto" // 포스터 이미지 사이즈 조정 및 중앙 정렬
           />
-          <div className="w-full text-xl font-black text-center mt-4">
-            결제 정보
-          </div>
+          <div className="w-full text-xl font-black mt-4">결제 정보</div>
           <div className="flex flex-col w-full mt-2.5">
             <div className="border-t border-b border-black">
               <div className="flex justify-between items-center py-3">
                 <div className="text-xs font-black">공연명</div>
-                <div className="text-sm font-black text-right">
+                <div className="text-xs font-black text-right">
                   {shortInfo?.title || "-"}
                 </div>
               </div>
               <div className="flex justify-between items-center py-3">
                 <div className="text-xs font-black">관람일시</div>
-                <div className="text-sm font-semibold text-right">
+                <div className="text-xs font-semibold text-right">
                   {shortInfo?.date || "-"}
                 </div>
               </div>
               <div className="flex justify-between items-center py-3">
                 <div className="text-xs font-black">장소</div>
-                <div className="text-sm font-semibold text-right">
+                <div className="text-xs font-semibold text-right">
                   {shortInfo?.location || "-"}
                 </div>
               </div>
@@ -71,13 +69,21 @@ const BillingPreviewPage = () => {
           <div className="text-xl font-black">선택된 좌석</div>
           {selectedSeats.map((seat, index) => (
             <div key={index} className="mt-2">
-              <span className="text-xs font-bold">좌석 번호:</span>{" "}
-              {seat.seatNumber},
-              <span className="text-xs font-bold"> 타입:</span> {seat.type},
-              <span className="text-xs font-bold"> 가격:</span>{" "}
-              {(seat.price || 0).toLocaleString()}원
+              <span className="text-xs font-bold">좌석 번호:</span>
+              <span className="text-xs font-bold">{seat.seatNumber},</span>
+
+              <span className="text-xs font-bold"> 타입:</span>
+              <span className="text-xs font-bold">{seat.type},</span>
+
+              <span className="text-xs font-bold"> 가격:</span>
+              <span className="text-xs font-bold">
+                {(seat.price || 0).toLocaleString()}원
+              </span>
             </div>
           ))}
+          <div className="mt-2 font-semibold">
+            선택된 좌석의 총 가격: {totalSeatsPrice.toLocaleString()}원
+          </div>
         </div>
       )}
 
@@ -88,19 +94,16 @@ const BillingPreviewPage = () => {
             {selectedTickets.map((ticket, index) => (
               <div key={index} className="mt-2 flex justify-between">
                 <span className="text-xs font-bold">{ticket.userType}:</span>
-                <span>
+                <span className="text-xs ">
                   {(ticket.price || 0).toLocaleString()}원 x {ticket.count}매
                 </span>
               </div>
             ))}
-            <div className="mt-2 font-semibold">
-              선택된 좌석의 총 가격: {totalSeatsPrice.toLocaleString()}원
-            </div>
           </div>
           <div className="w-full mt-4 border-t pt-2">
             <div className="flex justify-between">
               <span className="text-xs font-bold">총 금액 / 매수:</span>
-              <span>
+              <span className="text-xs">
                 {(totalPrice || 0).toLocaleString()}원 /{" "}
                 {selectedTickets.reduce((acc, ticket) => acc + ticket.count, 0)}
                 매
