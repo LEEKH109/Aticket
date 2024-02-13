ea8da08b1caf6a9bf60bcdb87d5c466672be4d65
이건희 <dlrjsgml109@gmail.com>
2024-02-14 06:28:24 +0900

    feat(fe): 결제 상세 페이지 디자인 초안

--- client/src/pages/book/BillingPreviewPage.jsx
+++ client/src/pages/book/BillingPreviewPage.jsx
@@ -4,8 +4,14 @@ import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
 const BillingPreviewPage = () => {
   const { state } = useLocation();
-  const { tid, redirectUrlPc, redirectUrlMobile, selectedSeats, shortInfo } =
-    state || {};
+  const {
+    redirectUrlPc,
+    redirectUrlMobile,
+    selectedSeats,
+    shortInfo,
+    selectedTickets,
+    totalPrice,
+  } = state || {};
 
   useEffect(() => {}, []);
 
@@ -15,6 +21,9 @@ const BillingPreviewPage = () => {
     window.location.href = targetUrl;
   };
 
+  const showInfo = shortInfo !== undefined;
+  const showTickets = selectedTickets !== undefined && totalPrice !== undefined;
+
   return (
     <div className="flex flex-col h-[calc(100vh-64px)] overflow-auto">
       <div className="w-full">
@@ -22,54 +31,64 @@ const BillingPreviewPage = () => {
           <ArrowBackIosIcon />
           <div>Aːticket</div>
         </div>
-        <div className="flex gap-0 px-5 text-xs font-bold text-center text-white mb-4">
-          <div className="justify-center px-6 py-2 whitespace-nowrap bg-black rounded-md border border-white border-solid ">
-            1.
-          </div>
-          <div className="grow justify-center px-12 py-2 bg-black rounded-md border border-white border-solid">
-            2. 결제 상세
-          </div>
-          <div className="justify-center px-6 py-2 whitespace-nowrap bg-black rounded-md border border-white border-solid ">
-            3.
-          </div>
-        </div>
-        <div className="p-4">
-          <h1>결제 상세</h1>
-          {shortInfo && (
-            <div className="mb-4">
-              <h2 className="text-lg font-semibold">{shortInfo.title}</h2>
-              <img
-                src={shortInfo.posterUrl}
-                alt="Event Poster"
-                className="w-32 h-auto my-2"
-              />
-              <p>장소: {shortInfo.location}</p>
+        {showInfo && (
+          <div className="flex flex-col px-5 text-black max-w-[310px]">
+            <div className="w-full text-xl font-black">결제 정보</div>
+            <div className="flex gap-5 justify-between px-3.5 pt-4 pb-3 mt-2.5 w-full border-t border-b border-solid bg-white bg-opacity-0 border-y-black">
+              <div className="flex flex-col self-start text-xs font-black whitespace-nowrap">
+                <div>공연명</div>
+                <div className="mt-5">관람일시</div>
+                <div className="mt-5">장소</div>
+              </div>
+              <div className="flex flex-col text-sm text-right">
+                <div className="flex flex-col pl-14">
+                  <div className="font-black">{shortInfo?.title || "-"}</div>
+                  <div className="mt-5 font-semibold">
+                    {shortInfo?.date || "-"}
+                  </div>
+                </div>
+                <div className="mt-5 font-semibold">
+                  {shortInfo?.location || "-"}
+                </div>
+              </div>
             </div>
-          )}
-          <p>Transaction ID: {tid}</p>
-          <div>
-            <h2>Selected Seats:</h2>
-            <ul>
-              {selectedSeats &&
-                selectedSeats.map((seat, index) => (
-                  <li key={index}>
-                    좌석 번호: {seat.seatNumber}, 타입: {seat.type}, 가격:{" "}
-                    {seat.price}원
-                  </li>
-                ))}
-            </ul>
+            {showTickets &&
+              selectedTickets.map((ticket, index) => (
+                <div
+                  key={index}
+                  className="flex gap-5 justify-between px-3.5 pt-4 pb-2 w-full border-b border-solid bg-white bg-opacity-0 border-b-black"
+                >
+                  <div className="text-xs font-black">{ticket.userType}</div>
+                  <div className="flex gap-5 justify-between text-sm font-semibold text-right">
+                    <div>{ticket.price.toLocaleString()} 원</div>
+                    <div>{ticket.count} 매</div>
+                  </div>
+                </div>
+              ))}
+            {showTickets && (
+              <div className="flex gap-5 justify-between p-3.5 w-full border-b border-solid bg-white bg-opacity-0 border-b-black">
+                <div className="text-xs font-black">총 금액 / 매수</div>
+                <div className="flex gap-5 justify-between text-sm font-semibold text-right">
+                  <div>{totalPrice.toLocaleString()} 원</div>
+                  <div>
+                    {selectedTickets.reduce(
+                      (acc, ticket) => acc + ticket.count,
+                      0
+                    )}{" "}
+                    매
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
-          <p>
-            총 금액: {selectedSeats.reduce((acc, seat) => acc + seat.price, 0)}
-            원
-          </p>
-          <button
-            onClick={handlePaymentRedirect}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            결제하기
-          </button>
-        </div>
+        )}
+
+        <button
+          onClick={handlePaymentRedirect}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          결제하기
+        </button>
       </div>
     </div>
   );
