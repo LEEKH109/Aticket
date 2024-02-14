e8d8a17b082ff5ff63381924a933355c620b82d5
이건희 <dlrjsgml109@gmail.com>
2024-02-14 15:44:46 +0900

    fix(fe): dateFormatter 로직 수정

--- client/src/util/dateFormatter.js
+++ client/src/util/dateFormatter.js
@@ -1,19 +1,15 @@
-const dateFormmatterWithTime = (dateTimeString) => {
-  const dateTimeFormat = dateTimeString.includes("T")
-    ? dateTimeString
-    : dateTimeString.replace(" ", "T");
-  const parsedDate = new Date(`${dateTimeFormat}+09:00`);
+function dateFormmatterWithTime(dateTimeString) {
+  const normalizedString = dateTimeString.replace("T", " ");
 
+  const [date, time] = normalizedString.split(" ");
+  const [year, month, day] = date.split("-");
+  const [hour, minute] = time.split(":");
+
+  const tempDate = new Date(year, month - 1, day);
   const week = ["일", "월", "화", "수", "목", "금", "토"];
-  const dayLabel = week[parsedDate.getDay()];
-
-  const year = parsedDate.getFullYear();
-  const month = ("0" + (parsedDate.getMonth() + 1)).slice(-2);
-  const day = ("0" + parsedDate.getDate()).slice(-2);
-  const hour = ("0" + parsedDate.getHours()).slice(-2);
-  const minute = ("0" + parsedDate.getMinutes()).slice(-2);
+  const dayLabel = week[tempDate.getDay()];
 
   return `${year}.${month}.${day}(${dayLabel}) ${hour}:${minute}`;
-};
+}
 
 export { dateFormmatterWithTime };
