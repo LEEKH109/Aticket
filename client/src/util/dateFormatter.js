ec0b2c2984f3ac5851578bf6ce70c122fd7ff1aa
이건희 <dlrjsgml109@gmail.com>
2024-02-14 15:09:10 +0900

    Update client/src/util/dateFormatter.js

--- client/src/util/dateFormatter.js
+++ client/src/util/dateFormatter.js
@@ -1,21 +1,28 @@
-const dateFormatter = (date) => {
-  const year = date.getFullYear();
-  const month = ("0" + (date.getMonth() + 1)).slice(-2);
-  const day = ("0" + date.getDate()).slice(-2);
-
-  return year + "." + month + "." + day;
-};
-
 const dateFormmatterWithTime = (date) => {
   const week = ["일", "월", "화", "수", "목", "금", "토"];
-  const year = date.getFullYear();
-  const month = ("0" + (date.getMonth() + 1)).slice(-2);
-  const day = ("0" + date.getDate()).slice(-2);
-  const dayLabel = week[date.getDay()];
-  const hour = ("0" + date.getHours()).slice(-2);
-  const minute = ("0" + date.getMinutes()).slice(-2);
+  const [datePart, timePart] = dateString.split(" ");
+  const [year, month, day] = datePart
+    .split("-")
+    .map((num) => parseInt(num, 10));
+  const [hour, minute, second] = timePart
+    .split(":")
+    .map((num) => parseInt(num, 10));
 
-  return year + "." + month + "." + day + "(" + dayLabel + ") " + hour + ":" + minute;
+  const date = new Date(year, month - 1, day, hour, minute, second);
+
+  const formattedDate = [
+    date.getFullYear(),
+    ("0" + (date.getMonth() + 1)).slice(-2),
+    ("0" + date.getDate()).slice(-2),
+  ].join(".");
+
+  const dayLabel = week[date.getDay()];
+  const formattedTime = [
+    ("0" + date.getHours()).slice(-2),
+    ("0" + date.getMinutes()).slice(-2),
+  ].join(":");
+
+  return `${formattedDate}(${dayLabel}) ${formattedTime}`;
 };
 
-export { dateFormatter, dateFormmatterWithTime };
+export { dateFormmatterWithTime };
