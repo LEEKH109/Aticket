f42250778814f93e16ed1e20fbd521016b7ee51c
최은비 <ii319@naver.com>
2024-02-14 12:05:58 +0900

    feat(fe): 컬렉션 이미지 크기 동일하게 css 수정

--- client/src/components/profile/CollectionItem.jsx
+++ client/src/components/profile/CollectionItem.jsx
@@ -2,13 +2,16 @@ import { Link } from "react-router-dom";
 
 const CollectionItem = ({ collection, index }) => {
   return (
-    <Link to={`/collection?index=${index}`} className="h-48">
+    <Link to={`/collection?index=${index}`} className="block relative h-48">
       {collection.type == "VIDEO" ? (
-        <video className="w-full h-full object-cover">
+        <video className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full object-cover">
           <source src={collection.mediaUrl} type="video/mp4" />
         </video>
       ) : (
-        <img src={collection.mediaUrl} />
+        <img
+          src={collection.mediaUrl}
+          className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full object-cover"
+        />
       )}
     </Link>
   );
