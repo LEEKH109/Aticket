a45c70c19cf2f9409b297c62c44ca2e9a88ba29d
최지원 <jiwonq98@naver.com>
2024-02-14 20:38:10 +0900

    style(fe): 채팅 관련 css 수정, 오타 수정

--- client/src/pages/ChatRoomPage.jsx
+++ client/src/pages/ChatRoomPage.jsx
@@ -11,6 +11,16 @@ const useGlobalStyles = () => {
     const style = document.createElement("style");
     style.type = "text/css";
     style.innerHTML = `
+
+    @keyframes fadeIn {
+      from {
+        opacity: 0;
+      }
+      to {
+        opacity: 1;
+      }
+    }
+    
         ::-webkit-scrollbar {
           display: none; /* Hide scrollbar for Chrome, Safari and Opera */
         }
@@ -61,7 +71,7 @@ const ChatRoom = () => {
     {
       categoryId: 3,
       category: "PLAY",
-      name: "공연",
+      name: "연극",
     },
   ];
 
@@ -84,8 +94,9 @@ const ChatRoom = () => {
     navigate("/chat");
   };
 
+  const fadeInClass = "animate-fadeIn";
+
   const onChatlogReceived = (message) => {
-    console.log("새 채팅이 들어왔다"); //message.body는 문자열 상태
     const newChatlog = JSON.parse(message.body).data;
     console.log(newChatlog);
     setPins((prevPins) => [...prevPins, newChatlog]);
@@ -100,7 +111,9 @@ const ChatRoom = () => {
     setLoading(true);
     try {
       const response = await ChatApi.chatScroll(category, page);
-      const newPins = response.data.data.content.sort((a, b) => a.chatlogId - b.chatlogId);
+      const newPins = response.data.data.content.sort(
+        (a, b) => a.chatlogId - b.chatlogId
+      );
       setPins((prevPins) => [...newPins, ...prevPins]);
       setHasMoreLogs(newPins.length === 15);
     } catch (error) {
@@ -138,7 +151,9 @@ const ChatRoom = () => {
 
   useEffect(() => {
     const connect = () => {
-      const stompClient = Stomp.over(() => new SockJS("http://i10a704.p.ssafy.io:8081/ws"));
+      const stompClient = Stomp.over(
+        () => new SockJS("http://i10a704.p.ssafy.io:8081/ws")
+      );
       stompClient.connect(
         {},
         () => {
@@ -182,7 +197,11 @@ const ChatRoom = () => {
           content: chatContent,
           regDate: new Date().toISOString(),
         };
-        client.current.send(`/app/send/${category}`, {}, JSON.stringify(chatlog));
+        client.current.send(
+          `/app/send/${category}`,
+          {},
+          JSON.stringify(chatlog)
+        );
         setChatContent("");
       } catch (error) {
         console.error("유저정보 가져오기 실패", error);
@@ -194,45 +213,74 @@ const ChatRoom = () => {
     <div>
       <div className="mb-2.5">
         <div
-          className={`flex items-center py-3 rounded-lg shadow-md bg-gradient-to-r ${getCategoryGradient(category)}`}
+          className={`flex items-center py-3 rounded-lg shadow-md bg-gradient-to-r ${getCategoryGradient(
+            category
+          )}`}
         >
           <IconButton onClick={handleBackClick} className="text-white">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="white">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="white"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M15 19l-7-7 7-7"
+              />
             </svg>
           </IconButton>
           <h1 className="text-xl font-bold text-center text-white">
             &nbsp;&nbsp;{getCategoryName(category)} 단체 채팅방
           </h1>
         </div>
-        <div ref={chatAreaRef} className="overflow-auto mb-4" style={{ height: "calc(100svh - 196px)" }}>
+        <div
+          ref={chatAreaRef}
+          className="overflow-auto mb-4"
+          style={{ height: "calc(100svh - 196px)" }}
+        >
           {pins.length > 0 ? (
             pins.map((chatlog) => (
               <div
                 key={chatlog.chatlogId}
-                className={`flex ${chatlog.userId === Number(nowLoginUser) ? "justify-end" : "justify-start"} mb-4`}
+                className={`flex ${
+                  chatlog.userId === Number(nowLoginUser)
+                    ? "justify-end"
+                    : "justify-start"
+                } mb-4 ${fadeInClass}`}
               >
                 <div className="max-w-md">
                   {chatlog.userId !== Number(nowLoginUser) && (
                     <div className="mb-1">
-                      <span className="text-sm font-semibold text-gray-800">{chatlog.nickname}</span>
+                      <span className="text-sm font-semibold text-gray-800">
+                        {chatlog.nickname}
+                      </span>
                     </div>
                   )}
                   <div
                     className={`p-4 rounded-lg ${
-                      chatlog.userId === Number(nowLoginUser) ? "bg-blue-500 text-white" : "bg-gray-200 text-gray-800"
+                      chatlog.userId === Number(nowLoginUser)
+                        ? "bg-indigo-500 text-white"
+                        : "bg-gray-200 text-gray-800"
                     } w-80`}
                   >
                     <p className="break-words">{chatlog.content}</p>
                   </div>
                   <div className="text-right">
-                    <span className="text-xs text-gray-500">{new Date(chatlog.regDate).toLocaleString()}</span>
+                    <span className="text-xs text-gray-500">
+                      {new Date(chatlog.regDate).toLocaleString()}
+                    </span>
                   </div>
                 </div>
               </div>
             ))
           ) : (
-            <p className="text-center text-gray-500">채팅이 존재하지 않습니다.</p>
+            <p className="text-center text-gray-500">
+              채팅이 존재하지 않습니다.
+            </p>
           )}
         </div>
         {isLogin ? (
@@ -251,7 +299,12 @@ const ChatRoom = () => {
               onClick={sendChat}
               className="ml-3 w-10 h-10 flex items-center justify-center rounded-full bg-blue-500 text-white hover:bg-blue-600 transition duration-150 ease-in-out"
             >
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="h-6 w-6"
+              >
                 <path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"></path>
               </svg>
             </button>
