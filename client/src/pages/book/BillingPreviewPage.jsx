195aa180d72b6291f03557afa722a2c7a8ef39d8
이건희 <dlrjsgml109@gmail.com>
2024-02-14 10:30:47 +0900

    style(fe): 로고 수정

--- client/src/pages/book/BillingPreviewPage.jsx
+++ client/src/pages/book/BillingPreviewPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
-import kakaopayImage from "./assets/kakaopay.png";
+import kakaopayImage from "../../assets/kakaopay.png";
 
 const BillingPreviewPage = () => {
   const { state } = useLocation();
